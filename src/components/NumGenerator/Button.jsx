398e9cdb4cd241da7cb1d6ac6050fbc317894b10
KerimGurbaz <kerim.grbz.01@gmail.com>
2022-10-18 02:02:53 +0200

    updated

--- src/components/NumGenerator/Button.jsx
+++ src/components/NumGenerator/Button.jsx
@@ -4,17 +4,7 @@ import { useState } from "react";
 const Button = ({ setValue, value }) => {
   const [click, setClick] = useState("");
 
-  // const handleValue = (e) => {
-  //   console.log(e.target.value);
-  //   setValue(e.target.value);
-  // };
-
-  // const handleClick = () => {
-  //   handleValue();
-  // };
   console.log(click, value);
-  console.log("click :>> ", click);
-  console.log("value :>> ", value);
 
   return (
     <div className="d-flex justify-content-center my-3">
